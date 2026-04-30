1963c1329e49298a3e9afe562ecbc49f80a89527
Colin Page <colinjpage20@gmail.com>
2026-04-30 16:03:21 -0400

    SingltonSO

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -36,6 +36,33 @@ public class SingletonPersistent<T> : Singleton<T> where T : Component
         DontDestroyOnLoad(Instance);
     }
 }
+public class SingletonSO<T> : ScriptableObject where T : SingletonSO<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                T[] assets = Resources.LoadAll<T>("");
+                if(assets == null || assets.Length < 1)
+                {
+                    Debug.LogError("Failed to find SingletonSO");
+                    return null;
+                }
+                else if(assets.Length > 1)
+                {
+                    Debug.LogWarning("Found too many instances of the SingletonSO. Returning first one...");
+                }
+                instance = assets[0];
+            }
+
+            return instance;
+        }
+    }
+
+}
 //public class SingletonNetwork<T> : NetworkBehaviour where T : Component
 //{
 //    public static T Instance { get; private set; }
