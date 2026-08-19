99de3c69847e3417735406592fa5bd5705fc1a81
Colin Page <colinjpage20@gmail.com>
2026-08-19 19:39:33 -0400

    Make IVariableAccess use the EventBroadcaster interfaces instead of the lower ISubscribable<Action> interfaces

--- Runtime/VariableSO/IVariableAccess.cs
+++ Runtime/VariableSO/IVariableAccess.cs
@@ -3,7 +3,7 @@ using System;
 
 // Has a value that be gotten or subscribed/unsubscribed
 // but does NOT allow setting or other modification
-public interface IVariableAccess<T> : ISubscribable<Action<T>>, ISubscribable<Action>
+public interface IVariableAccess<T> : EventBroadcaster<T>, EventBroadcaster
 {
     public T Value => GetValue();
     public T GetValue();
